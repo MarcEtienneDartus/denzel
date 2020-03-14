8fa38a01a0c05ff8d846f2a4f494a6139a7b238c
MarcEtienneDartus <marcetienne.dartus@gmail.com>
2020-03-14 16:35:37 +0100

    Render Random Movie

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppBar, Typography, Toolbar, Paper } from '@material-ui/core';
+import { Container, Row, Col} from 'react-bootstrap';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [movie, setMovie] = useState(undefined);
+
+  useEffect(() => {
+    fetch('http://localhost:9292/movies/')
+    .then(res => res.json())
+    .then(json => setMovie(json))
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">
+            Denzel
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {
+        movie && <Container>
+        <Row className="row">
+          <Col md={8} sm={12}>
+            <Paper elevation={3} className="info" >
+              <Typography variant="h3" >{movie.title}</Typography>
+              <Typography variant="h5" className="space">{movie.synopsis}</Typography>
+              <Typography variant="h6">Année : {movie.year}</Typography>
+              <Typography variant="h6">Note : {movie.rating}</Typography>
+              <Typography variant="h6">Nombre de vote : {movie.votes}</Typography>
+            </Paper>
+          </Col>
+          <Col md={4} sm={12}>
+            <img src={movie.poster}/>
+          </Col>
+        </Row>
+      </Container>
+      }
+  </>
   );
 }
 
